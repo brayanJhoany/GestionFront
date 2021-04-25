d11b9c822bf7bb1da1f769916979dd2fbd6efcfc
brayan escobar <brayan.escobar@live.com>
2021-04-24 22:01:25 -0400

    rutas para bitácora, syllabus y plan de clases

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,34 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Administrador from "../views/Administrador.vue";
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Administrador from '../views/Administrador.vue';
+
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/",
-    name: "Administrador",
-    component: () =>
-      import(
-        /* webpackChunkName: "Administrador" */ "@/views/Administrador.vue"
-      ),
+    path: '/',
+    name: 'Administrador',
+    component: () => import(/* webpackChunkName: "Administrador" */ '@/views/Administrador.vue'),
     children: [
       {
-        path: "/curso/:id/:nombre/seccion/:seccion",
-        name: "Curso",
-        component: () =>
-          import(
-            /* webpackChunkName: "cursos" */ "@/components/Globales/BodyGlobal.vue"
-          ),
-        children: [],
+        path: '/curso/:id/:nombre/seccion/:seccion',
+        name: 'Curso',
+        component: () => import(/* webpackChunkName: "cursos" */ '@/components/Globales/BodyGlobal.vue'),
+      },
+      {
+        path: '/curso/:id/:nombre/seccion/:seccion/bitacora',
+        name: 'Bitacora',
+        component: () => import(/* webpackChunkName: "cursos" */ '@/components/Bodys/BodyBitacora.vue'),
+      },
+      {
+        path: '/curso/:id/:nombre/seccion/:seccion/syllabus',
+        name: 'Syllabus',
+        component: () => import(/* webpackChunkName: "cursos" */ '@/components/Bodys/BodySyllabus.vue'),
+      },
+      {
+        path: '/curso/:id/:nombre/seccion/:seccion/plan-de-clases',
+        name: 'PlanDeClases',
+        component: () => import(/* webpackChunkName: "cursos" */ '@/components/Bodys/BodyPlanDeClases.vue'),
       },
     ],
   },
