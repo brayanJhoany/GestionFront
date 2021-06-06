039913ed38f4e6d47db532100b783be56c94e6de
Felipe Milla <felipe.milla@haulmer.com>
2021-06-05 22:24:41 -0400

    vista agregar/eliminar/modificar actividades
    this

--- src/components/Bodys/BodyPlanDeClases.vue
+++ src/components/Bodys/BodyPlanDeClases.vue
@@ -76,14 +76,96 @@
               </div>
             </v-card-title>
           </v-card>
-
-          <v-data-table
+          <div class="mb-6"></div>
+          <v-col
+            v-for="(item, i) in items"
+            :key="i"
+            class=""
+            style="padding: 0px"
+          >
+            <v-card color="success" class="mb-1">
+              <v-card-title class="headline text--center" primary-title>
+                <div class="v-markdown">
+                  <h5 class="white--text difuminado">
+                    Semana: {{ item.semana }} - {{ item.fecha }}
+                  </h5>
+                </div>
+                <v-spacer></v-spacer>
+                <div style="text-align: right">
+                  <v-tooltip bottom color="primary">
+                    <template v-slot:activator="{ on }">
+                      <v-btn
+                        v-on="on"
+                        color="white"
+                        fab
+                        small
+                        depressed
+                        class="mr-2 py-2"
+                        @click="editarActividad(item)"
+                      >
+                        <v-icon color="primary"> fas fa-edit </v-icon>
+                      </v-btn>
+                    </template>
+                    <span><strong>Editar</strong></span>
+                  </v-tooltip>
+                  <v-tooltip bottom color="primary">
+                    <template v-slot:activator="{ on }">
+                      <v-btn
+                        v-on="on"
+                        color="white"
+                        fab
+                        small
+                        depressed
+                        class="mr-2 py-2"
+                        @click="eliminarActividad(item)"
+                      >
+                        <v-icon color="warning"> fas fa-trash-alt </v-icon>
+                      </v-btn>
+                    </template>
+                    <span><strong>Eliminar</strong></span>
+                  </v-tooltip>
+                </div>
+              </v-card-title>
+              <v-container style="background-color: white; padding: 5px">
+                <v-row style="margin: 0; padding: 0">
+                  <v-col md="12" lg="4" style="margin: 0; padding: 0">
+                    <h5 class="mt-2 mb-1 ml-3">Actividad</h5>
+                    <div
+                      class="DIV mb-2"
+                      style="height: 80px; overflow: auto; font-size: 90%"
+                    >
+                      {{ item.actividad }}
+                    </div>
+                  </v-col>
+                  <v-col md="12" lg="4" style="margin: 0; padding: 0">
+                    <h5 class="mt-2 mb-1 ml-3">Saber/Tema</h5>
+                    <div
+                      class="DIV mb-2"
+                      style="height: 80px; overflow: auto; font-size: 90%"
+                    >
+                      {{ item.proposito }}
+                    </div>
+                  </v-col>
+                  <v-col md="12" lg="4" style="margin: 0; padding: 0">
+                    <h5 class="mt-2 mb-1 ml-3">Observación</h5>
+                    <div
+                      class="DIV mb-2"
+                      style="height: 80px; overflow: auto; font-size: 90%"
+                    >
+                      {{ item.observacion }}
+                    </div>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-card>
+          </v-col>
+          <!--<v-data-table
             :headers="headers"
             :items="desserts"
             :items-per-page="5"
             class="elevation-2 mt-4"
           >
-          </v-data-table>
+          </v-data-table>-->
         </section>
       </v-col>
       <v-col cols="12" md="2"> </v-col>
@@ -116,6 +198,222 @@
     </v-row>
 
     -->
+    <v-dialog v-model="dialogAgregarActividad" persistent max-width="500px">
+      <v-card class="mx-auto" max-width="500">
+        <v-card-title class="headline primary text--center" primary-title>
+          <h5 class="white--text">Agregar actividad</h5>
+        </v-card-title>
+        <v-container class="px-5 mt-5">
+          <v-form
+            ref="form_añadirActividad"
+            style="margin: 0; padding: 0"
+            v-model="form_añadirActividadValido"
+            lazy-validation
+          >
+            <v-select ref="semana" :items="semanas" label="Semana"></v-select>
+            <v-menu
+              ref="menu"
+              v-model="menu"
+              :close-on-content-click="false"
+              transition="scale-transition"
+              offset-y
+              min-width="290px"
+            >
+              <template v-slot:activator="{ on }">
+                <v-text-field v-model="fechaAddObs" readonly v-on="on">
+                </v-text-field>
+              </template>
+              <v-date-picker
+                ref="picker"
+                v-model="fechaAddObs"
+                :max="new Date().toISOString().substr(0, 10)"
+                min="1950-01-01"
+                @change="save"
+                @input="menu = false"
+              ></v-date-picker>
+            </v-menu>
+            <v-textarea
+              color="secondary"
+              label="Actividad"
+              :rules="[
+                (v) => !!v || 'La actividad es requerida.',
+                (v) =>
+                  /^[a-zA-Z0-9,-.¿?:;/¡!\u00f1\u00d1 ]+$/.test(v) ||
+                  'La actividad contiene caracteres no permitidos.',
+              ]"
+            ></v-textarea>
+            <v-textarea
+              color="secondary"
+              label="Saber/Tema"
+              :rules="[
+                (v) => !!v || 'El saber/tema es requerida.',
+                (v) =>
+                  /^[a-zA-Z0-9,-.¿?:;/¡!\u00f1\u00d1 ]+$/.test(v) ||
+                  'El saber/tema contiene caracteres no permitidos.',
+              ]"
+            ></v-textarea>
+            <v-textarea
+              color="secondary"
+              label="Observación"
+              :rules="[
+                (v) => !!v || 'La observación es requerida.',
+                (v) =>
+                  /^[a-zA-Z0-9,-.¿?:;/¡!\u00f1\u00d1 ]+$/.test(v) ||
+                  'La observación contiene caracteres no permitidos.',
+              ]"
+            ></v-textarea>
+            <div class="pb-1" style="text-align: right">
+              <v-btn
+                :small="$vuetify.breakpoint.smAndDown ? true : false"
+                rounded
+                color="warning"
+                @click="dialogAgregarActividad = false"
+              >
+                <h4 class="white--text">Cancelar</h4>
+              </v-btn>
+              <v-btn
+                :disabled="!form_añadirObservacionValido"
+                :small="$vuetify.breakpoint.smAndDown ? true : false"
+                rounded
+                color="secondary"
+                class="ml-2"
+                :loading="cargando"
+                @click=""
+              >
+                <h4 class="white--text">Agregar</h4>
+              </v-btn>
+            </div>
+          </v-form>
+        </v-container>
+      </v-card>
+    </v-dialog>
+
+    <v-dialog v-model="dialogEditarActividad" persistent max-width="500px">
+      <v-card class="mx-auto" max-width="500">
+        <v-card-title class="headline primary text--center" primary-title>
+          <h5 class="white--text">Editar actividad</h5>
+        </v-card-title>
+        <v-container class="px-5 mt-5">
+          <v-form
+            ref="form_añadirActividad"
+            style="margin: 0; padding: 0"
+            v-model="form_editarActividadValido"
+            lazy-validation
+          >
+            <v-select ref="semana" :items="semanas" label="Semana"></v-select>
+            <v-menu
+              ref="menu"
+              v-model="menu"
+              :close-on-content-click="false"
+              transition="scale-transition"
+              offset-y
+              min-width="290px"
+            >
+              <template v-slot:activator="{ on }">
+                <v-text-field v-model="fechaAddObs" readonly v-on="on">
+                </v-text-field>
+              </template>
+              <v-date-picker
+                ref="picker"
+                v-model="fechaAddObs"
+                :max="new Date().toISOString().substr(0, 10)"
+                min="1950-01-01"
+                @change="save"
+                @input="menu = false"
+              ></v-date-picker>
+            </v-menu>
+            <v-textarea
+              color="secondary"
+              label="Actividad"
+              :rules="[
+                (v) => !!v || 'La descripción es requerida.',
+                (v) =>
+                  /^[a-zA-Z0-9,-.¿?:;/¡!\u00f1\u00d1 ]+$/.test(v) ||
+                  'La descripción contiene caracteres no permitidos.',
+              ]"
+            ></v-textarea>
+            <v-textarea
+              color="secondary"
+              label="Saber/Tema"
+              :rules="[
+                (v) => !!v || 'La descripción es requerida.',
+                (v) =>
+                  /^[a-zA-Z0-9,-.¿?:;/¡!\u00f1\u00d1 ]+$/.test(v) ||
+                  'La descripción contiene caracteres no permitidos.',
+              ]"
+            ></v-textarea>
+            <v-textarea
+              color="secondary"
+              label="Observación"
+              :rules="[
+                (v) => !!v || 'La descripción es requerida.',
+                (v) =>
+                  /^[a-zA-Z0-9,-.¿?:;/¡!\u00f1\u00d1 ]+$/.test(v) ||
+                  'La descripción contiene caracteres no permitidos.',
+              ]"
+            ></v-textarea>
+            <div class="pb-1" style="text-align: right">
+              <v-btn
+                :small="$vuetify.breakpoint.smAndDown ? true : false"
+                rounded
+                color="warning"
+                @click="dialogEditarActividad = false"
+              >
+                <h4 class="white--text">Cancelar</h4>
+              </v-btn>
+              <v-btn
+                :disabled="!form_añadirObservacionValido"
+                :small="$vuetify.breakpoint.smAndDown ? true : false"
+                rounded
+                color="secondary"
+                class="ml-2"
+                :loading="cargando"
+                @click=""
+              >
+                <h4 class="white--text">Agregar</h4>
+              </v-btn>
+            </div>
+          </v-form>
+        </v-container>
+      </v-card>
+    </v-dialog>
+
+    <v-dialog v-model="dialogEliminarActividad" persistent max-width="500px">
+      <v-card class="mx-auto" max-width="500px">
+        <v-card-title class="headline primary text--center" primary-title>
+          <h5 class="white--text">Eliminar actividad</h5>
+        </v-card-title>
+
+        <v-card-title
+          class="text-justify"
+          :style="
+            $vuetify.breakpoint.smAndDown
+              ? 'font-size: 90%;'
+              : 'font-size: 100%;'
+          "
+          >Esta seguro que desea eliminar la actividad?</v-card-title
+        >
+        <div class="px-5 mt-5 pb-4" style="text-align: right">
+          <v-btn
+            :small="$vuetify.breakpoint.smAndDown ? true : false"
+            rounded
+            color="warning"
+            @click="dialogEliminarActividad = false"
+          >
+            <h4 class="white--text">Cancelar</h4>
+          </v-btn>
+          <v-btn
+            :small="$vuetify.breakpoint.smAndDown ? true : false"
+            rounded
+            color="secondary"
+            class="ml-2"
+            @click=""
+          >
+            <h4 class="white--text">Eliminar</h4>
+          </v-btn>
+        </div>
+      </v-card>
+    </v-dialog>
   </v-container>
 </template>
 
@@ -130,8 +428,104 @@ export default {
   Volvername: "PlanDeClases",
   data() {
     return {
+      items: [
+        {
+          id: 1,
+          semana: "1",
+          fecha: "05/06/2021",
+          proposito: "aprender java",
+          actividad: "esta es una actividad bla bla bla",
+          observacion: "esta es una observacion bla bla bla",
+        },
+        {
+          id: 2,
+          semana: "2",
+          fecha: "05/06/2021",
+          proposito: "aprender java",
+          actividad: "esta es una actividad bla bla bla",
+          observacion: "esta es una observacion bla bla bla",
+        },
+        {
+          id: 3,
+          semana: "2",
+          fecha: "05/06/2021",
+          proposito: "aprender java",
+          actividad: "esta es una actividad bla bla bla",
+          observacion: "esta es una observacion bla bla bla",
+        },
+        {
+          id: 4,
+          semana: "2",
+          fecha: "05/06/2021",
+          proposito: "aprender java",
+          actividad: "esta es una actividad bla bla bla",
+          observacion: "esta es una observacion bla bla bla",
+        },
+        {
+          id: 5,
+          semana: "2",
+          fecha: "05/06/2021",
+          proposito: "aprender java",
+          actividad: "esta es una actividad bla bla bla",
+          observacion: "esta es una observacion bla bla bla",
+        },
+        {
+          id: 6,
+          semana: "2",
+          fecha: "05/06/2021",
+          proposito: "aprender java",
+          actividad: "esta es una actividad bla bla bla",
+          observacion: "esta es una observacion bla bla bla",
+        },
+        {
+          id: 7,
+          semana: "2",
+          fecha: "05/06/2021",
+          proposito: "aprender java",
+          actividad: "esta es una actividad bla bla bla",
+          observacion: "esta es una observacion bla bla bla",
+        },
+        {
+          id: 8,
+          semana: "2",
+          fecha: "05/06/2021",
+          proposito: "aprender java",
+          actividad: "esta es una actividad bla bla bla",
+          observacion: "esta es una observacion bla bla bla",
+        },
+        {
+          id: 9,
+          semana: "2",
+          fecha: "05/06/2021",
+          proposito: "aprender java",
+          actividad: "esta es una actividad bla bla bla",
+          observacion: "esta es una observacion bla bla bla",
+        },
+        {
+          id: 10,
+          semana: "2",
+          fecha: "05/06/2021",
+          proposito: "aprender java",
+          actividad: "esta es una actividad bla bla bla",
+          observacion: "esta es una observacion bla bla bla",
+        },
+      ],
       dialogAgregarActividad: false,
       form_añadirActividadValido: false,
+      dialogEditarActividad: false,
+      form_editarActividadValido: false,
+      dialogEliminarActividad: false,
+      activePicker: null,
+      date: null,
+      fechaAddObs: new Date().toISOString().substr(0, 10),
+      fechaUpObs: new Date().toISOString().substr(0, 10),
+      menu: false,
+      menu2: false,
+      landscape: false,
+      semanas: [
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+        21, 22, 23, 24, 25, 26, 27,
+      ],
       headers: [
         { text: "Fecha", align: "start", value: "name", sortable: false },
         { text: "Saber/Tema", value: "calories", sortable: false },
@@ -202,6 +596,15 @@ export default {
       ],
     };
   },
+
+  methods: {
+    editarActividad(item) {
+      this.dialogEditarActividad = true;
+    },
+    eliminarActividad(item) {
+      this.dialogEliminarActividad = true;
+    },
+  },
 };
 </script>
 
@@ -220,10 +623,6 @@ export default {
   border-color: color;
 }
 .DIV {
-  border: 1px solid rgba(0, 0, 0, 0.4);
-  border-radius: 5px;
-  -moz-border-radius: 5px;
-  -webkit-border-radius: 5px;
   padding: 10px;
 }
 </style>
